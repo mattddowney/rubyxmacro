633451e53566e0b5e9d81130507f5249c8cd4da5
Matthew Downey <mattddowney@gmail.com>
2011-01-04 18:35:04 -0700

    added comment to sendKeys method

--- lib/rubyxmacro.rb
+++ lib/rubyxmacro.rb
@@ -2,6 +2,7 @@ require 'rubyxmacro/rubyxmacro'
 
 module RubyXMacro
   class Display
+    # Press and release a string of keys.
     def sendKeys(keyString)
       keyString.split('').each do |key|
         self.keyPress(key)
